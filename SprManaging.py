ba4a8b494ef88fd017968f53d1f303c2e8711709
DecibHell <pierrick.chauvet@gmail.com>
2014-05-09 16:21:25 +0200

    Mise en forme de SprManaging.py

--- SprManaging.py
+++ SprManaging.py
@@ -56,10 +56,16 @@ def loadSprites():
     SG.charR = cutSprite(sprites,64,32,32,32)
     SG.charU = cutSprite(sprites,96,32,32,32)
 
+    SG.charRW = cutSprite(sprites,64,0,32,32)
+    SG.charLW = cutSprite(sprites,32,0,32,32)
+    SG.charUW = cutSprite(sprites,96,0,32,32)
+    SG.charDW = cutSprite(sprites,0,0,32,32)
+
     SG.wall = cutSprite(sprites,0,64,32,32)
     SG.floor = cutSprite(sprites,0,96,32,32)
     SG.door = cutSprite(sprites,32,64,32,32)
     SG.key = cutSprite(sprites,32,96,32,32)
+    SG.button = cutSprite(sprites,64,128,96,32)
 
     SG.mobD = cutSprite(sprites,64,64,32,32)
     SG.mobL = cutSprite(sprites,96,96,32,32)
@@ -71,8 +77,6 @@ def loadSprites():
     SG.mobRW = cutSprite(sprites,96,192,32,32)
     SG.mobLW = cutSprite(sprites,96,224,32,32)
 
-    SG.button = cutSprite(sprites,64,128,96,32)
-
     SG.whipH = cutSprite(sprites,0,160,32,32)
     SG.whipD = cutSprite(sprites,0,192,32,32)
     SG.whipU = cutSprite(sprites,0,128,32,32)
@@ -81,11 +85,6 @@ def loadSprites():
     SG.whipEndD = cutSprite(sprites,32,160,32,32)
     SG.whipEndR = cutSprite(sprites,32,192,32,32)
     SG.whipEndL = cutSprite(sprites,32,224,32,32)
-
-    SG.charRW = cutSprite(sprites,64,0,32,32)
-    SG.charLW = cutSprite(sprites,32,0,32,32)
-    SG.charUW = cutSprite(sprites,96,0,32,32)
-    SG.charDW = cutSprite(sprites,0,0,32,32)
 
     SG.border = cutSprite(background,0,0,192,352)
     SG.line = cutSprite(background,0,0,96,32)
