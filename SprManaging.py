21b0522ee8991ee99c3abc9a8a82323dec0c658a
DecibHell <pierrick.chauvet@gmail.com>
2014-05-09 15:55:03 +0200

    Merge de deux fichiers

--- SprManaging.py
+++ SprManaging.py
@@ -1,5 +1,39 @@
 from PIL import Image , ImageTk
-from SprGlobals import SG
+
+# Liste des variables "sprites"
+class SG:
+    title=None
+    charD=None
+    charL=None
+    charU=None
+    charR=None
+    charDW=None
+    charLW=None
+    charUW=None
+    charRW=None
+    wall=None
+    floor=None
+    door=None
+    key=None
+    mobD=None
+    mobU=None
+    mobL=None
+    mobR=None
+    mobDW=None
+    mobUW=None
+    mobLW=None
+    mobRW=None
+    button=None
+    border=None
+    line=None
+    bottom=None
+    whipD=None
+    whipH=None
+    whipU=None
+    whipEndD=None
+    whipEndL=None
+    whipEndU=None
+    whipEndR=None
 
 # Extrait un sprite de la page des sprites, aux coords (x,y) et de taille (width, height)
 def cutSprite(sprites,y,x,width,height):
